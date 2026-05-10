e46a67558f7612b8d3ff4cb7ec67d884d6c849c4
BradyBassett <bradydbassett@gmail.com>
2026-05-09 19:03:21 -0500

    Remove placeholder weather API endpoints and related code

--- SubTaskerBackend/Program.cs
+++ SubTaskerBackend/Program.cs
@@ -58,36 +58,10 @@ WebApplication app = builder.Build();
 app.UseExceptionHandler();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
-// TODO - remove this once we have actual endpoints
-app.MapGet("/api/weather", () =>
-{
-    string[] summaries =
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
-        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    var forecast = Enumerable.Range(1, 5)
-        .Select(index => new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 56),
-            summaries[Random.Shared.Next(summaries.Length)]))
-        .ToArray();
-
-    return Results.Ok(forecast);
-})
-.WithName("GetTestWeather")
-.WithTags("Test")
-.RequireAuthorization();
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
