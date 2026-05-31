a04a2227419bd6c11284964d6b010c0d2015af6d
BradyBassett <bradydbassett@gmail.com>
2026-05-31 14:59:40 -0500

    Implement User uniqueness constraints; update indexes for Tags and Categories to enforce unique combinations of UserId and Name.

---
+++ SubTaskerBackend/Migrations/20260531193043_UserUniqueness.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SubTaskerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_Name",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Name",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_UserId_Name",
+                table: "Tags",
+                columns: new[] { "UserId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId_Name",
+                table: "Categories",
+                columns: new[] { "UserId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_UserId_Name",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId_Name",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+        }
+    }
+}
